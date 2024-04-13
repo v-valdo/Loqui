6b806cc21337b0f0b60490c6365be405b15fec3b
VValdo <v.sersam@gmail.com>
2024-04-13 21:16:12 +0200

    basic css

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Loqui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
